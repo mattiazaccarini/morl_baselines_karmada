e73bc9c1a4f30a582c43747688f27900fdc0d1b2
Filippo Poltronieri <filippopoltronieri@192.168.1.59>
2025-06-27 14:26:03 +0200

    File for testing

---
+++ gym-multi-k8s/run_cnsm_mpmoqlearning.py
@@ -0,0 +1,76 @@
+import envs
+import gymnasium as gym
+import mo_gymnasium as mo_gymnasium
+from morl_baselines.common.scalarization import tchebicheff
+from morl_baselines.multi_policy.multi_policy_moqlearning.mp_mo_q_learning import MPMOQLearning
+import pickle, time
+
+import numpy as np
+
+num_envs = 4
+
+GAMMA = 0.9
+TOTAL_TIMESTEPS = 150000
+EVAL_FREQ = 500
+
+def scalarization_fn(v, w):
+    return tchebicheff(tau=4.0, reward_dim=4)(v, w)
+
+
+if __name__ == "__main__":
+
+    number_of_clusters = [4, 6, 8, 10, 12, 14, 16]
+
+    for num_clusters in number_of_clusters:
+        env = mo_gymnasium.make("karmada-scheduling-multi-v1", num_clusters=num_clusters,
+                                 min_replicas=1, max_replicas=16, 
+                                 file_results_name=f"karmada_gym_{num_clusters}components_results_mpmoqlearning_{time.time()}",
+                                 is_eval_env=False,)
+        eval_env = mo_gymnasium.make("karmada-scheduling-multi-v1", num_clusters=num_clusters,
+                                      min_replicas=1, max_replicas=16,
+                                      file_results_name=f"karmada_gym_{num_clusters}components_results_eval_mpmoqlearning_{time.time()}",
+                                      is_eval_env=True)
+
+        ref_point = np.array([
+            0.0,
+            0.0,
+            0.0,
+            0.0005
+        ], dtype=np.float32)
+
+        agent = MPMOQLearning(
+            env,
+            learning_rate=0.3,
+            scalarization=scalarization_fn,
+            use_gpi_policy=False,
+            dyna=False,
+            initial_epsilon=1,
+            final_epsilon=0.01,
+            epsilon_decay_steps=int(2e5),
+            weight_selection_algo="random",
+            epsilon_ols=0.1,
+        )
+
+        agent.train(
+            total_timesteps=TOTAL_TIMESTEPS, 
+            timesteps_per_iteration=15000, 
+            eval_freq=EVAL_FREQ,
+            eval_env=eval_env,
+            num_eval_weights_for_front=100,      # ⬅ weights to estimate full Pareto front
+            num_eval_episodes_for_front=5,       # ⬅ episodes per weight for full front
+            ref_point=ref_point,
+        )
+
+    
+        model_data = {
+            "policies": agent.policies,
+            "ref_point": ref_point,
+            #"weights": agent.get,
+            #"pareto_front": agent.pareto_front,
+        }
+        
+        # Get timestamp for the model file
+        timestamp = time.time()
+
+        with open(f"mpmoqlearning_model_{timestamp}_{num_clusters}.pkl", "wb") as f:
+            pickle.dump(model_data, f)
