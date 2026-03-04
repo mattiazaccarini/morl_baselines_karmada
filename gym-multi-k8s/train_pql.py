20cdbe07d33ffaed9853e05dce9b4be3e486d0f6
mattiazaccarini <zccmtt@unife.it>
2026-03-04 13:50:57 +0100

    Little modifications to test some discrete environments with PQL and GeoPQL

--- gym-multi-k8s/train_pql.py
+++ gym-multi-k8s/train_pql.py
@@ -3,6 +3,7 @@ import gymnasium as gym
 import mo_gymnasium as mo_gymnasium
 from algorithms.pql import PQL
 from algorithms.geometric_pql import GeometricPQL
+from algorithms.geometric_pql_4_obj import GeometricPQL4
 from wrappers.discretized_wrapper import DiscretizerWrapper
 
 import os, pickle, wandb
@@ -14,19 +15,21 @@ GAMMA = 0.99
 TOTAL_TIMESTEPS = 10000
 EVAL_FREQ = 500
 
+num_clusters = 8  # You can change this to test with different cluster sizes
+
 if __name__ == "__main__":
-    env = mo_gymnasium.make("karmada-scheduling-multi-v0", num_clusters=4, 
-                            min_replicas=1, max_replicas=16, 
+    env = mo_gymnasium.make("karmada-scheduling-multi-v1", num_clusters=num_clusters, 
+                            min_replicas=8, max_replicas=8, 
                             file_results_name="karmada_gym_multi_train_results", is_eval_env=False)
     env = DiscretizerWrapper(env, n_bins=8) # Not required for MOQLearning
-    ref_point = np.array([600, 10, 0.9])
+    ref_point = np.array([0.0, 0.0, 0.0, 0.0005], dtype=np.float32)  # Reference point for hypervolume calculation
 
-    eval_env = mo_gymnasium.make("karmada-scheduling-multi-v0", num_clusters=4, min_replicas=1, 
-                                 max_replicas=16, file_results_name="karmada_gym_multi_eval_results", is_eval_env=True)
+    eval_env = mo_gymnasium.make("karmada-scheduling-multi-v1", num_clusters=num_clusters, min_replicas=8, 
+                                 max_replicas=8, file_results_name="karmada_gym_multi_eval_results", is_eval_env=True)
     eval_env = DiscretizerWrapper(eval_env, n_bins=8)
 
     #os.makedirs("checkpoints", exist_ok=True)
-    #model_path = ("checkpoints/karmada_morl_pql.pkl")
+    #model_path = (f"checkpoints/karmada_morl_geometric_pql_{num_clusters}_clusters.pkl")
 
     agent = PQL(
         env=env,
@@ -36,6 +39,13 @@ if __name__ == "__main__":
     )
 
     #agent = GeometricPQL(
+    #    env,
+    #    gamma=GAMMA,
+    #    ref_point=ref_point,
+    #    log=True, # use weights and biases to see the results!
+    #)
+
+    #agent = GeometricPQL4(
     #    env,
     #    gamma=GAMMA,
     #    ref_point=ref_point,
